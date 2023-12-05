20106e14c957a907eeb4bd9455b60c7bbe28c8f5
rheinpoedwin <146629682+rheinpoedwin@users.noreply.github.com>
2023-12-05 19:44:16 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,30 @@
+    main()
+def generate_random_data():
+
+if __name__ == "__main__":
+
+def main():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+import random
+    for item in data:
+    return data
+        print(f"Random Number: {item}")}
+    }
+}
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+function main() {
+
+        console.log("Random Number: " + item);
+    for (let i = 0; i < 10; i++) {
+main();
+    const data = [];
+function generateRandomData() {
+    const data = generateRandomData();
+
+    });
+
+    return data;
