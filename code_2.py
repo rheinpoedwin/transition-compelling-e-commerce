e03506fb2c7849b6cd7e7f6720724d939dccc40f
rheinpoedwin <146629682+rheinpoedwin@users.noreply.github.com>
2023-12-08 23:05:12 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,13 @@
+        for (int item : data) {
+        int[] data = new int[10];
+public class RandomDataGenerator {
+
+        }
+            System.out.println("Random Number: " + item);
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        for (int i = 0; i < 10; i++) {
+
+    public static void main(String[] args) {
+}
+    }
