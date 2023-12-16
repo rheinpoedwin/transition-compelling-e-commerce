684ec7bc4b7420922ddd106a900a57c3b83726ae
rheinpoedwin <146629682+rheinpoedwin@users.noreply.github.com>
2023-12-16 07:06:48 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,26 +1,68 @@
-        }
-        for (int i = 0; i < 10; i++) {
-}
-
-            data[i] = (int) (Math.random() * 100) + 1;
-        for (int item : data) {
-    }
-public class RandomDataGenerator {
-        int[] data = new int[10];
-            System.out.println("Random Number: " + item);
-
-        }
-    public static void main(String[] args) {
+        print(f"Random Number: {item}")
+def main():
+if __name__ == "__main__":
+    data = generate_random_data()
     return data
 
-    data = generate_random_data()
 import random
-    main()
+
 def generate_random_data():
+    main()
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    for item in data:
+
+    main()
+        print(f"Random Number: {item}")
+import random
+
+def main():
+    data = generate_random_data()
+
+    return data
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+if __name__ == "__main__":
+def generate_random_data():    data = [random.randint(1, 100) for _ in range(10)]
 
 def main():
         print(f"Random Number: {item}")
+import random
+    main()
 
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
+    data = generate_random_data()
+
+    return data
+
+def generate_random_data():
+    for item in data:    data.forEach(item => {
+
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+main();
+
+    const data = [];
+function generateRandomData() {
+    });
+        console.log("Random Number: " + item);
+}
+    }
+function main() {
+
+    return data;
+}}
+    }
+
+public class RandomDataGenerator {
+
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+    public static void main(String[] args) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+        }
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
